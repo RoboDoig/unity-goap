cee76a64c9a4a3c3290a0fb1a1bd471860149cfd
Andrew Erskine <erskine94@gmail.com>
2021-01-24 19:05:50 -0800

    Idle state

--- Assets/FSM/IncreaseEnergyFSM.cs
+++ Assets/FSM/IncreaseEnergyFSM.cs
@@ -15,7 +15,7 @@ public class IncreaseEnergyFSM : StateMachineBehaviour
                         () => {animator.SetTrigger("failure");}
                       );
     }
-
+    
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
     //override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     //{
